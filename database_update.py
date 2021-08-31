6e3ab76c1dbef562bccdfd16c082d9a4b0ef486d
sourav4949 <46929852+sourav4949@users.noreply.github.com>
2021-08-31 22:56:44 +0530

    added the create delete and update statement for databases

---
+++ database_update.py
@@ -0,0 +1,21 @@
+import mysql.connector
+def update(id):
+    conn = mysql.connector.connect(host='localhost', database='mydb', user='root', password='Test12345')
+    if conn.is_connected():
+        print("connected to mysql db")
+        cursor=conn.cursor()
+        str="update emp set sal= 12225 where id ='%d'"
+        # sal=(12225)
+        args=(id)
+        try:
+            print("Inside try block")
+            cursor.execute(str %args)
+            conn.commit()
+            print("employee updated")
+        except:
+            conn.rollback
+        finally:
+            cursor.close()
+            conn.close()
+
+update(3)
