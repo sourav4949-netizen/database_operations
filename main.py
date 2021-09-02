61892eb538631788b441986ab471e7a2cbb453de
sourav4949-netizen <66011644+sourav4949-netizen@users.noreply.github.com>
2021-09-02 12:48:01 +0530

    database conn procedure
    added some more information on the database connection procedures

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ Read
 Update
 Delete
 
-Install connector
-import the module into our programmer
-then create a connection for the database and
-then using the connection we can create a cursor object.
-using the cursor we can read and write on the database
+The different steps involved in connecting to a database and do some basic operations are:
+Install connector - we have to install a connector in our venv for connecting to different database we have different connector.
+import the module into our programme. importing the connector to our programe
+then create a connection for the database using the credentials, we can also create a config files and import it as a module in our program to hide the creadentials from users.
+then using the connection we have to create a cursor object - This cursor object contains all the data that we get from a database.
+So, if we are running a read command on our database then cursor will contain all the data, then we are going to fetch the data from cursor using fetchone() or 
+fetchall() command.
+fetchone() will fetch only a single record at a time while fetchall() will fetch all the records present in cursor.
+using the cursor we can read and write on the database.
+All the operations that we do on the database are executed through cursor.execute(str).
+The str contains the command to run on database. we can do CRUD operation as stated above.
 """
